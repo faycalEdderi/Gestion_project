8e043dc48bffc1bd9d4de44a924ac3dfdfad5736
fedderi <fedderi@assystemtechnologies.com>
2019-09-26 16:09:07 +0200

    front + js progressbar fonction

---
+++ expleoWebsite/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+
+def Uos(request):
+    ...
+    template = loader.get_template('Uos.html')
+    return HttpResponse(template.render(request=request))
+
+#def Uos(request):
+
+    #return render(request,'Uos.html')
+
+def profil(request):
+
+    return render(request,'pages/profil.html')
+
+def diagramme(request):
+
+    return render(request,'pages/diagramme.html')
+
+def pointages(request):
+
+    return render(request,'pages/pointages.html')
+
+def Uo(request):
+
+    return render(request,'pages/Uo.html')
+
+def historique(request):
+
+    return render(request,'pages/historique.html')
+
